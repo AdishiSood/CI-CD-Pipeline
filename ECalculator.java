55102f8cbd966aae4737eda6f366da45783e6e4a
Adishi <adishisood@gmail.com>
2022-06-09 22:31:45 +0530

    Add files via upload

--- ECalculator.java
+++ ECalculator.java
@@ -1,36 +1,41 @@
 package ecalculator;
 
+import java.util.logging.Logger;
+
 public class ECalculator {
 
+	static Logger l = Logger.getLogger(ECalculator.class.getName());
 	static void add(int a, int b)		
 		{
 			int result = a+b;
-			System.out.println("After adding a and b : "+ result);
+			l.info("After adding a and b : "+ result);
 		}
 	static void sub(int a, int b)		
 	{
 		int result = a-b;
-		System.out.println("After substracting a and b : "+ result);
+		l.info("After substracting a and b : "+ result);
 	}
 	static void mul(int a, int b)		
 	{
 		int result = a*b;
-		System.out.println("After multiplying a and b : "+ result);
+		l.info("After multiplying a and b : "+ result);
 	}
 	static void div(int a, int b)		
 	{
 		int result = a/b;
-		System.out.println("After dividing a and b : "+ result);
+		l.info("After dividing a and b : "+ result);
 	}
 	
 
-public static void main(String[] args)
+public static void main(String args[])
 {
+	//Logger l = Logger.getLogger(ECalculator.class.getName());
 	int a=20, b=10;
 	System.out.println("The value of a and b: "+a+","+b);
 	ECalculator.add(a,b);
 	ECalculator.sub(a,b);
 	ECalculator.mul(a,b);
 	ECalculator.div(a,b);
+	
 }
 }
